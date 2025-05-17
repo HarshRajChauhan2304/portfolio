4e9086fb81a59111da3555848875a4c7ca7be3a6
HarshRajChauhan2304 <152949798+HarshRajChauhan2304@users.noreply.github.com>
2025-05-17 13:17:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.black').onclick = () => {
+    alert("Let's get in touch!");
+  };
+  
+  document.querySelector('.orange').onclick = () => {
+    alert("Showing experience...");
+  };
